7dfdfbe7382ed94c069fd5647bfa97924aceed93
IamSanmeet <97578872+IamSanmeet@users.noreply.github.com>
2023-03-21 20:59:19 -0400

    working

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -1,29 +1,8 @@
 extends Control
 
-export var indicator_margin = Vector2(25, 15)
-export var indicator_index = 25
-onready var Indicator = load("res://UI/Indicator.tscn")
-
 func _ready():
-	update_score()
-	update_time()
-	update_lives()
+	var _changed = Global.connect("changed",self,"_on_score_changed")
+	_on_score_changed()
 
-
-func update_score():
+func _on_score_changed():
 	$Score.text = "Score: " + str(Global.score)
-
-func update_time():
-	$Time.text = "Time: " + str(Global.time)
-
-func update_lives():
-	var indicator_pos = Vector2(indicator_margin.x, Global.VP.y - indicator_margin.y)
-	for i in $Indicator_Container.get_children():
-		i.queue_free()
-	for i in range(Global.lives):
-		var indicator = Indicator.instance()
-		indicator.position = Vector2(indicator_pos.x + i*indicator_index, indicator_pos.y)
-		$Indicator_Container.add_child(indicator)
-
-func _on_Timer_timeout():
-	Global.update_time(-1)
